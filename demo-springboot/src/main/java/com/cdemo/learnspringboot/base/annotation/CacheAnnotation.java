ff211fd5744ed6061e4ce05039c57b3d91f09adb
colstars <yxx6762@qq.com>
2022-11-13 13:11:37 +0800

    update

--- demo-springboot/src/main/java/com/cdemo/learnspringboot/base/annotation/CacheAnnotation.java
+++ demo-springboot/src/main/java/com/cdemo/learnspringboot/base/annotation/CacheAnnotation.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({java.lang.annotation.ElementType.METHOD, ElementType.TYPE})
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface CacheAnnotation {
 
     String app() default "colstar";
