987d189a22cb2d0c1b0a1bf1435d1ce52b473b3d
colstars <yxx6762@qq.com>
2023-08-09 20:18:29 +0800

    update

--- demo-common-dal/src/main/java/com/cdemo/dal/model/UserInfo.java
+++ demo-common-dal/src/main/java/com/cdemo/dal/model/UserInfo.java
@@ -1,17 +1,10 @@
 package com.cdemo.dal.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
 /**
  * @description:
  * @author: star
  * @time: 2021/3/7 9:00
  */
-@Data
-@AllArgsConstructor
-@RequiredArgsConstructor
 public class UserInfo {
     private int id;
     private String name;
@@ -19,4 +12,44 @@ public class UserInfo {
 
     private int startPage;
     private int endPage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStartPage() {
+        return startPage;
+    }
+
+    public void setStartPage(int startPage) {
+        this.startPage = startPage;
+    }
+
+    public int getEndPage() {
+        return endPage;
+    }
+
+    public void setEndPage(int endPage) {
+        this.endPage = endPage;
+    }
 }
